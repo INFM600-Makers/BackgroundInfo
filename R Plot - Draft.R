3aaa7aba01e31ddd20cab733b598a3e3be778958
AnkitDhallGit <42852743+AnkitDhallGit@users.noreply.github.com>
2018-12-13 16:55:41 -0500

    Update R Plot - Draft.R

--- R Plot - Draft.R
+++ R Plot - Draft.R
@@ -33,20 +33,45 @@ ggplot(LAFinal,aes(LAFinal$trip_route_category,LAFinal$passholder_type))+geom_co
 #Q3. How do busy hours affect the Bicycle patterns?
 #For this question and the sheer vastness of the probably factors, we have at first taken the summary() #function to find the busiest start and end stations for LA to determine the stations which need to be most #populated with bicycles.
 
-summary(LAFinal)
 
+#Following are additional plots for contextual research and analysis of the system. Overall, the following plots are responsible for relations between
+#member type, total bike usage, route type, gender distribution and age distribution along with relation between ridership and temperature as well
+
+ggplot() + geom_col(data=LAFinal, aes(x=LAFinal$Date, y = LAFinal$Duration/360), color="darkcyan") + geom_line(data=LAWeather, aes(x=LAWeather$Date, y = LAWeather$Avg.Temp),size =2, color="red") + labs(x="Month", y="Duration of Trips/ Avg. Temperature", title = "LA - Trip Duration (Mutiple of 6 hours) Distribution vs Temperature Distribution")
+
+ggplot() + geom_col(data=DCFinal, aes(x=DCFinal$Date, y = DCFinal$Duration/1800), color="darkcyan") + geom_line(data=DCWeather, aes(x=DCWeather$Date, y = DCWeather$Avg.Temp),size =2, color="red") + labs(x="Month", y="Duration of Trips/ Avg. Temperature", title = "DC - Trip Duration (Mutiple of 60 hours) Distribution vs Temperature Distribution")
+
+ggplot(LAFinal, aes(LAFinal$Member.type)) + geom_bar(fill = "#008081") +labs(title = "LA - Bike Usage by Customer Type", x="Customer Type", y="Usage Count")
+
+ggplot(DCFinal, aes(DCFinal$Member.type)) + geom_bar(fill = "#008081") +labs(title = "DC - Bike Usage by Customer Type", x="Customer Type", y="Usage Count")
+
+ggplot(CGFinal, aes(CGFinal$Member.type)) + geom_bar(fill = "#008081") +labs(title = "CG - Bike Usage by Customer Type", x="Customer Type", y="Usage Count")
+
+ggplot(LAFinal,aes(LAFinal$Trip.route,LAFinal$Member.type))+geom_jitter(col="#008081",show.legend = F, size = 0.5) + labs(x="Route Category", y="Pass Holder", title = "LA - Membership Distribution based on Route")
+
+ggplot(LAFinal, aes(LAFinal$Month, LAFinal$Hour))+ geom_count(col="maroon",show.legend = F) + labs(x="Month", y='Hours of Day', title = "Monthly Bike Usage - Hourly Distribution")
+
+ggplot(DCFinal, aes(DCFinal$WDAY, DCFinal$Hour))+ geom_count(col="#008081",show.legend = F) + labs(x="Day of Week", y='Hour of Day', title = "DC - Weekly Bike Usage - Hourly Distribution")
+
+ggplot(LAFinal,aes(LAFinal$Member.type,LAFinal$Duration))+geom_jitter(col="#008081",show.legend = F) + labs(x="Member Type", y='Trip Duration', title = "LA - Trip Duration and Membership Relationship")
+
+
+
+DCBikesAvailable<-length(unique(DCFinal$Bike.number))
+LABikesAvailable<-length(unique(LAFinal$Bike.number))
+CGBikesAvailable<-length(unique(CGFinal$Bike.number))
+
+#Summary is used to find most popular spots in the cities and google maps are used to plot these spots for visualization
+summary(LAFinal)
 #Top 5 popular start stations :
- #Ocean Front Walk & Navy               
- #7th & Flower                                    
- #Ocean Front Walk & North Venice         
+#Ocean Front Walk & Navy               
+#7th & Flower                                    
+#Ocean Front Walk & North Venice   
+#Union Station West Portal
 #Main & 1st                            
 #Top 5 popular end stations :
 #Downtown Santa Monica Expo Line Station 
- #Ocean Front Walk & Navy      
- #7th & Flower          
+#Ocean Front Walk & Navy      
+#7th & Flower          
 #Union Station West Portal            
- #Ocean Front Walk & North Venice     
-
-#Notes:
-#1. Interpretation has not been included with the plots as we feel that it is more relevant to the final #presentation and not the current draft for R-Plots.
-#2. We are trying to produce spatial plots which use Google APIs. We have faced an issue with that as #now the API is paid only. The API is available through Google Cloud Platform which has a significant #learning curve and hence might take a little more time
+#Ocean Front Walk & North Venice     
